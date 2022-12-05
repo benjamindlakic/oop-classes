cddd7d4fa4dd2573ec46e81df47ee141fda2f21f
benjamindlakic <benjomaster7@gmail.com>
2022-12-05 15:02:39 +0100

    lab6

---
+++ lab6/BirdMain.java
@@ -0,0 +1,17 @@
+package lab6;
+
+public class BirdMain {
+    public static void main(String[] args) {
+        BirdWatchers db = new BirdWatchers();
+        loop: while(true) {
+            System.out.print("? ");
+            String option = db.input().toLowerCase().trim();
+            switch (option) {
+                case "add" -> db.addBird();
+                case "observation" -> db.observe();
+                case "statistics" -> db.statistics();
+                case "show" -> db.show();
+                case "quit" -> {break loop;}
+            }
+        }
+}
