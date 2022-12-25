375660b78c9463b9cf45f49a20452e9734aa4bc8
benjamindlakic <benjomaster7@gmail.com>
2022-12-25 22:14:49 +0100

    lab9

---
+++ lab9/MainSensor.java
@@ -0,0 +1,21 @@
+package lab9;
+
+public class MainSensor {
+    public static void main(String[] args) {
+        Sensor kumpola= new Thermometer(); 
+        Sensor kaisenemi= new Thermometer();
+        Sensor helsinkiVata= new Thermometer();
+
+        AverageSensor helsinkiArea= new AverageSensor();
+        helsinkiArea.addSensor(kumpola);
+        helsinkiArea.addSensor(kaisenemi);
+        helsinkiArea.addSensor(helsinkiVata);
+
+        helsinkiArea.on();
+        System.out.println("avg temp in helsinki is "+helsinkiArea.measure()+" degrees");
+        System.out.println("avg temp in helsinki is "+helsinkiArea.measure()+" degrees");
+        System.out.println("avg temp in helsinki is "+helsinkiArea.measure()+" degrees");
+
+        System.out.println("readings : " + helsinkiArea.readings());
+    }
+}
--- lab9/analysis/Main.java
+++ lab9/analysis/Main.java
@@ -10,6 +10,5 @@ public class Main {
         System.out.println("Lines: "+ analysis.lines());
         System.out.println("Characters: "+ analysis.characters());
     }
-    
 
 }
